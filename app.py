98455071699dc9eced82b5541c5fee82843d8c3b
nagendranice <60419186+nagendranice@users.noreply.github.com>
2022-10-21 14:56:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,36 @@ movies_list = pickle.load(open('movies-dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_list)
 similarity = pickle.load(open('similarity.pkl','rb'))
 st.title('Movie Recommender System')
+st.set_page_config(layout="wide")
 
+video_html = """
+		<style>
+
+		#myVideo {
+		  position: fixed;
+		  right: 0;
+		  bottom: 0;
+		  min-width: 100%; 
+		  min-height: 100%;
+		}
+
+		.content {
+		  position: fixed;
+		  bottom: 0;
+		  background: rgba(0, 0, 0, 0.5);
+		  color: #f1f1f1;
+		  width: 100%;
+		  padding: 20px;
+		}
+
+		</style>	
+		<video autoplay muted loop id="myVideo">
+		  <source src="https://static.streamlit.io/examples/star.mp4")>
+		  Your browser does not support HTML5 video.
+		</video>
+        """
+
+st.markdown(video_html, unsafe_allow_html=True)
 selected_movie_name = st.selectbox('Which movie do you like',movies['title'].values)
 
 if st.button('Recommend'):
