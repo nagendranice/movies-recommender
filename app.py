cb76cc280efed1a527bbac15bd60421e7e288e9e
nagendranice <60419186+nagendranice@users.noreply.github.com>
2022-10-21 15:09:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,34 +4,6 @@ import pandas as pd
 import requests
 #st.set_page_config(layout="wide")
 
-'''video_html = """
-		<style>
-
-		#myVideo {
-		  position: fixed;
-		  right: 0;
-		  bottom: 0;
-		  min-width: 100%; 
-		  min-height: 100%;
-		}
-
-		.content {
-		  position: fixed;
-		  bottom: 0;
-		  background: rgba(0, 0, 0, 0.5);
-		  color: #f1f1f1;
-		  width: 100%;
-		  padding: 20px;
-		}
-
-		</style>	
-		<video autoplay muted loop id="myVideo">
-		  <source src="https://www.pexels.com/video/a-woman-fixing-her-gloves-7322211/")>
-		  Your browser does not support HTML5 video.
-		</video>
-        """
-
-st.markdown(video_html, unsafe_allow_html=True)'''
 page_bg_img = '''
 <style>
 body {
